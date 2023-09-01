16aa12e1ed0f864f221a7cdb5385929e886d068f
PAVANEISHWARAN A L A RAVINDRAN <pavaneishwaran@gmail.com>
2023-09-01 18:01:55 +0800

    Class and Assignment View and Controller Addition

---
+++ Controllers/AcademicClassController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DDAC_System.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DDAC_System.Controllers
+{
+    [Authorize(Roles="Admin")]
+    public class AcademicClassController : Controller
+    {
+        private readonly System_ClassDBContext _context;
+
+        public AcademicClassController(System_ClassDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AcademicClass
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.AcademicClass.ToListAsync());
+        }
+
+        // GET: AcademicClass/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var academicClass = await _context.AcademicClass
+                .FirstOrDefaultAsync(m => m.Class_ID == id);
+            if (academicClass == null)
+            {
+                return NotFound();
+            }
+
+            return View(academicClass);
+        }
+
+        // GET: AcademicClass/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AcademicClass/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Class_ID,Class_Name,Class_Lecturer,ClassStartTime,ClassEndTime,Class_Location")] AcademicClass academicClass)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(academicClass);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(academicClass);
+        }
+
+        // GET: AcademicClass/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var academicClass = await _context.AcademicClass.FindAsync(id);
+            if (academicClass == null)
+            {
+                return NotFound();
+            }
+            return View(academicClass);
+        }
+
+        // POST: AcademicClass/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Class_ID,Class_Name,Class_Lecturer,ClassStartTime,ClassEndTime,Class_Location")] AcademicClass academicClass)
+        {
+            if (id != academicClass.Class_ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(academicClass);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AcademicClassExists(academicClass.Class_ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(academicClass);
+        }
+
+        // GET: AcademicClass/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var academicClass = await _context.AcademicClass
+                .FirstOrDefaultAsync(m => m.Class_ID == id);
+            if (academicClass == null)
+            {
+                return NotFound();
+            }
+
+            return View(academicClass);
+        }
+
+        // POST: AcademicClass/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var academicClass = await _context.AcademicClass.FindAsync(id);
+            _context.AcademicClass.Remove(academicClass);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AcademicClassExists(int id)
+        {
+            return _context.AcademicClass.Any(e => e.Class_ID == id);
+        }
+    }
+}
---
+++ Controllers/AssignmentController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DDAC_System.Models;
+
+namespace DDAC_System.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        private readonly System_AssignmentDBContext _context;
+
+        public AssignmentController(System_AssignmentDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Assignment
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Assignment.ToListAsync());
+        }
+
+        // GET: Assignment/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var assignment = await _context.Assignment
+                .FirstOrDefaultAsync(m => m.AssignmentID == id);
+            if (assignment == null)
+            {
+                return NotFound();
+            }
+
+            return View(assignment);
+        }
+
+        // GET: Assignment/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Assignment/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AssignmentID,AssignmentName,AssignmentDesc,AssignmentHandOut,AssignmentDue")] Assignment assignment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(assignment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(assignment);
+        }
+
+        // GET: Assignment/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var assignment = await _context.Assignment.FindAsync(id);
+            if (assignment == null)
+            {
+                return NotFound();
+            }
+            return View(assignment);
+        }
+
+        // POST: Assignment/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AssignmentID,AssignmentName,AssignmentDesc,AssignmentHandOut,AssignmentDue")] Assignment assignment)
+        {
+            if (id != assignment.AssignmentID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(assignment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AssignmentExists(assignment.AssignmentID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(assignment);
+        }
+
+        // GET: Assignment/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var assignment = await _context.Assignment
+                .FirstOrDefaultAsync(m => m.AssignmentID == id);
+            if (assignment == null)
+            {
+                return NotFound();
+            }
+
+            return View(assignment);
+        }
+
+        // POST: Assignment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var assignment = await _context.Assignment.FindAsync(id);
+            _context.Assignment.Remove(assignment);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AssignmentExists(int id)
+        {
+            return _context.Assignment.Any(e => e.AssignmentID == id);
+        }
+    }
+}
