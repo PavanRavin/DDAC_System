c15e259960e00493eae114ed530a8fce0eb5e11a
PAVANEISHWARAN A L A RAVINDRAN <pavaneishwaran@gmail.com>
2023-09-07 22:06:50 +0800

    Final Application without AWS

---
+++ Migrations/20230907111745_migrateToAWSTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DDAC_System.Migrations
+{
+    public partial class migrateToAWSTest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
